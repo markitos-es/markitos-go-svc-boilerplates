fbe1ba0c579b05893dff3d53086614a1999ca444
Marco Antonio - markitos <markitos.es.info@gmail.com>
2025-03-03 07:13:53 +0100

    - vo name tested

--- internal/domain/types.go
+++ internal/domain/types.go
@@ -1,6 +1,9 @@
 package domain
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
 type BoilerplateId struct {
 	value string
@@ -12,4 +15,23 @@ func NewBoilerplateId(value string) (*BoilerplateId, error) {
 	}
 
 	return nil, fmt.Errorf("invalid boilerplate id, must be an uuid v4, received: %s", value)
+}
+
+type BoilerplateName struct {
+	value string
+}
+
+func NewBoilerplateName(value string) (*BoilerplateName, error) {
+	if isValidBoilerplateName(value) {
+		return &BoilerplateName{value}, nil
+	}
+
+	return nil, fmt.Errorf("invalid boilerplate name, only letters and spaces, start/end with letter, received: %s", value)
+}
+
+func isValidBoilerplateName(value string) bool {
+	pattern := `^[a-zA-Z]{1}[a-zA-Z ]+[a-zA-Z]$|^[a-zA-Z]$`
+	matched, _ := regexp.MatchString(pattern, value)
+
+	return matched
 }
---
+++ testsuite/internal/domain/boilerplate.id_test.go
@@ -0,0 +1,40 @@
+package domain_test
+
+import (
+	"testing"
+
+	"github.com/markitos/markitos-svc-boilerplate/internal/domain"
+)
+
+func TestCanCreateValidBoilerplateName(t *testing.T) {
+	validNames := []string{
+		"ValidName",
+		"AnotherValidName",
+		"Valid Name With Spaces",
+		"Short",
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+		"InvalidNameWithMoreThanOneHundredCharactersInvalidNameWithMoreThanOneHundredCharactersInvalidNameWithMoreThanOneHundredCharacters"}
+	for _, name := range validNames {
+		if _, err := domain.NewBoilerplateName(name); err != nil {
+			t.Errorf("Expected valid name, but got invalid: %s", name)
+		}
+	}
+
+	invalidNames := []string{
+		" InvalidName",
+		"InvalidName ",
+		"Invalid Name ",
+		" Invalid Name",
+		"Invalid@Name",
+		"Invalid#Name",
+		"Invalid123Name",
+		"Invalid Name With Spaces ",
+		" Invalid Name With Spaces",
+		"Invalid Name With Spaces And Symbols!",
+	}
+	for _, name := range invalidNames {
+		if _, err := domain.NewBoilerplateName(name); err == nil {
+			t.Errorf("Expected valid name, but got invalid: %s", name)
+		}
+	}
+}
